cea3feefecdc69d000a0ca336a708ef574b848da
Deyser Orozco <deiser1999@gmail.com>
2021-10-11 18:49:01 -0500

    :zap: Added TailwindCSS and Page Splash

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-
+import { Platform } from '@ionic/angular';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+
+  constructor(
+    private platform: Platform,
+    private router: Router
+  ) {}
+
+
+  initializateApp() {
+    this.platform.ready().then( () => {
+      this.router.navigateByUrl('splash', { replaceUrl:true });
+    });
+  }
+
+
 }
